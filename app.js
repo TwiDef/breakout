641e1c40a698aecf959b5deb3f7cf77963cf8201
Def <deferenseo@gmail.com>
2023-01-10 22:55:44 +0300

    added posibility move user block

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const grid = document.querySelector('.grid');
 const blockWidth = 100;
 const blockHeight = 20;
-const userStart = [610, 10];
+const userStart = [540, 10];
+const boardWidth = 1200;
 let currentPosition = userStart;
 
 
@@ -48,6 +49,29 @@ addBlock();
 
 const user = document.createElement('div');
 user.classList.add('user');
-user.style.left = currentPosition[0] + 'px';
-user.style.bottom = currentPosition[1] + 'px';
+drawUser();
 grid.appendChild(user);
+
+function drawUser() {
+    user.style.left = currentPosition[0] + 'px';
+    user.style.bottom = currentPosition[1] + 'px';
+}
+
+function moveUser(e) {
+    switch (e.key) {
+        case 'ArrowLeft':
+            if (currentPosition[0] > 0) {
+                currentPosition[0] -= 20;
+                drawUser();
+            }
+            break;
+        case 'ArrowRight':
+            if (currentPosition[0] < boardWidth - blockWidth) {
+                currentPosition[0] += 20;
+                drawUser();
+            }
+
+            break;
+    }
+}
+document.addEventListener('keydown', moveUser);
